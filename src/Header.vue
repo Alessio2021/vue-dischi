4dcfc56c063f274ab25b0aa350a02eb1fb7895d2
Alessio2021 <alessiobut1911@gmail.com>
2022-01-17 15:29:17 +0100

    main

---
+++ src/Header.vue
@@ -0,0 +1,26 @@
+.<template>
+  <header>
+    <div class="row">
+        <div class="logo col-12">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="logo spotify">
+        </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        padding: 10px;
+        background-color: #2E3A46;
+        img {
+            width: 50px;
+        }
+    }
+
+</style>
---
+++ src/Main.vue
@@ -0,0 +1,23 @@
+.<template>
+  <main>
+    <div class="container-60">
+        <div class="row">
+            <div class="col-5">
+                
+            </div>
+        </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    main {
+
+    }
+</style>
