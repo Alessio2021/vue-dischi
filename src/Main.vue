34da1f2c21baafdee80cc68661ceb8f3dc543f8c
Alessio2021 <alessiobut1911@gmail.com>
2022-01-17 17:14:40 +0100

    main start

--- src/Main.vue
+++ src/Main.vue
@@ -1,9 +1,15 @@
 .<template>
   <main>
     <div class="container-60">
-        <div class="row">
-            <div class="col-5">
-                
+        <div class="row row-cols-5">
+            <div>
+                <img 
+                    src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" 
+                    alt=""
+                >
+                <h1>New Jersey</h1>
+                <h2>Bon Jovi</h2>
+                <h2>1988</h2>
             </div>
         </div>
     </div>
@@ -11,15 +17,37 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+    name: 'Main',
+    data() {
+        return {
+            cards: [],
 
+        }
+    },
+    mounted() {
+        this.getCards();
+    },
+    methods: {
+        getCards() {
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((result) => {
+                this.cards = result.data.results;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+        
+    },
 }
 </script>
 
 <style lang="scss" scoped>
     main {
         .container-60 {
-            
+            width: 60%;
         }
     }
 </style>
